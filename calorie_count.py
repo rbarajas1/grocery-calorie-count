fd3cf6c32011eac695b8432bb3920082e9d18d84
rbarajas1 <rubenbarajas321@gmail.com>
2025-02-08 10:07:23 -0600

    Add files via upload
    added main .py file that contains record of past grocery items purchased and dates

---
+++ calorie_count.py
@@ -0,0 +1,244 @@
+
+
+# LCC = low fat cottage cheese (24 oz whole container) total calories
+# CC = cottage cheese (24 oz whole container) total calories
+# PB = peanut butter (28 oz container) total calories
+# PN = peanuts (16 oz whole container) total calories
+# HM = Hummus (20 oz whole container) total calories
+# GY = Greek Yogurt (32 oz whole container) total calories
+# OL = Olives (12 oz container) total calories
+# EG = dozen eggs (extra large dutch farms) total calories
+# BN = banana calories per pound
+# GG = green grapes calories per pound
+# GP = green peas (12 oz bag) 
+# WP = white peaches calories per pound (68 cal per 175g)
+# SP = Sweet Potato calories per pound (103 cal per 114g)
+# LN = Lentils (28 servings 110 cal per servings)
+# MR = 16 oz white mushrooms (6 cal per oz)
+# BR = broccoli (153 cal per pound)
+# SB = 16oz strawberry
+# CU = cucumber (100grams = 15 cal) 1 large cucumber = 425 grams
+# SN = Spinach (100 grams = 23 cal) 1 bunch spinach = 210 grams
+# JI = Jicama (1 lb = 172 cal)
+# PI = Pineapple (1kg = 500cal)
+# CR = Carrots (1lb = 186cal)
+# PM = black plum (1lb = 210 cal)
+# WN = 16 oz walnuts (3040 cal = 16 oz)
+# BP = green bell pepper (91 cal per lb)
+# GA = golden apple (257 cal per lb)
+# NO = Navel Oranges (222 cal per lb)
+# CB = Chicken Breast (748 cal per lb)
+# BS = butternut squash (204 cal per lb)
+# ZS = zucchini squash (76 cal per lb)
+
+
+LCC = 540
+CC = 660
+PB = 4500
+PN = 2560
+HM = 1200
+GY = 600
+OL = 1000
+EG = 960
+BN = 404
+GG = 314.7
+GP = 280
+WP = 176.25
+SP = 409.82
+LN = 3080
+MR = 96
+BR = 153
+SB = 144
+CU = 15
+SN = 210
+JI = 38
+PI = 500
+CR = 186
+PM = 210
+WN = 3040
+BP = 91
+GA = 257
+NO = 222
+CB = 748
+BS = 204
+ZS = 76
+
+June2_24 = CC + PN + PB + 3.02*BN + 2.57*BN + CC + 3.03*WP
+June11_24 = 2.7*BN + 2.48*BN + GY + CC + 1.67*GG + 3.27*WP
+June15_24 = PN + GP + 1.84*GG + GY + CC + 3.64*WP + PB + 1.58*BN
+June17_24 = 1.94*GG + HM + 4.36*WP + GY + 4.06*BN + OL + CC
+June21_24 = 2.49*BN + 2.19*BN + EG + HM + PN + CC + OL + PB + 2.9*SP + GY
+June23_24 = HM + CC + GY + 5*BN + 1.79*GG + 4.44*WP
+June27_24 = 2.97*GG + LCC + GY + LN + 2.82*BN + PB + 5.39*WP + PN + OL
+July1_24 = EG + GY + 4.89*WP + 1.8*GG + 5.19*BN + CC + PN + PB
+July5_24 = 2.11*GG + LN + HM + LCC + GY + OL + 3.94*WP + (2.45+2.67)*BN
+July8_24 = HM + CC + GY + 2.5*GG + PB + 5.02*BN + 3.6*WP
+July11_24 = GY + HM + CC + 2.01*GG + 4.37*BN + 4.44*WP
+July15_24 = LCC + GY + PB + MR + 1.61*BR + 1.98*BR + 4.33*WP
+July18_24 = PN + 2.08*BN + 2.73*BN + 1.96*GG + GY + LCC + 4.77*WP + OL
+July21_24 = EG + GY + PB + HM + CC + 1.17*BR + 3.79*WP + 3.35*BN + 2.14*BN + MR + 2.49*GG + GP
+July24_24 = HM + GY + LCC + 2.58*GG + 3.73*WP + 4.43*BN
+July28_24 = 2.59*SP + 4.52*WP + 1.93*GG + PB + GP + HM + PN + LCC + GY + 2.39*BN + 1.57*BN
+July31_24 = SB + OL + GY + HM + LCC + 1.92*GG + 3.4*WP + 3.34*BN
+Aug3_24 = SB + 2.51*GG + 1.67*BN + 2.13*BN + HM + LCC + PB + GY + 4.7*WP
+Aug7_24 = SB + HM + LCC + LN + OL + 2.48*BN + GY + 2.71*GG + 2.7*BN + 5.71*WP
+Aug9_24 = 1.88*BN + 2.22*BN + MR + 1.26*BR + PN + LCC + GY + PB + 1.94*SP + 5.81*WP
+Aug11_24 = LCC + (3*CU*4.25) + 2.59*JI + 1.3*PI + GY + GP + SB + SN + 2.73*CR
+Aug15_24 = 2.33*SP + LN + GY + 5.33*BN + 0.89*JI + 5.29*WP + SB + LCC + 2.67*GG
+Aug18_24 = SB + PN + PB
+Aug19_24 = EG + 1.87*CR + (3*CU*4.25) + 2.35*GG + GY + LCC + OL + 4.58*BN + 4.26*WP
+Aug23_24 = BR + SN + 4.65*WP + 2.07*CR + 1.3*PI + LCC + 2.21*BN + 2.53*BN + (3*CU*4.25) + GY + SB 
+Aug27_24 = 2.4*PM + EG + WN + PN + LCC + PI + 4.48*BN + GY + PB + OL
+Aug29_24 = 5.24*BN + (3*CU*4.25) + 6.06*WP + LCC + GY + 2.19*GG
+Sep3_24 = GY + LCC + PI + 3.36*BN + 2.78*GG + 1.75*BN + 3.09*JI + 2.12*CR + 5.77*WP + SB
+Sep6_24 = 2.33*SP + 5*WP + OL + 1.67*BN + 2.17*BN + PB + LCC + GY + SN + GP
+Sep8_24 = 1.54*JI + 1.47*JI + 1.22*BR + SB + (3*CU*4.25) + 2.36*GG
+Sep11_24 = 1.82*JI + LN + GY + LCC + 3.52*BN + 1.38*BP + 2.84*GA + (2*CU*4.25) + 1.86*CR + SN
+Sep14_24 = GY + EG + OL + LCC + 4.04*BN + 3.28*GA + (2*CU*4.25) + 1.82*CR + 1.26*BP + SB + 1.61*JI
+Sep17_24 = LCC + GY + 2.72*BN + 1.19*BP + PN + PB + 2.99*GA + 2.39*GG + 1.52*CR + 1.85*JI
+Sep21_24 = SN + LCC + 3.88*BN + OL + SB + 1.38*BN + 1*BP + (2*CU*4.25) + 2.96*JI + 1.36*CR
+Sep25_24 = GY + LN + LCC + 2.26*GG + 3.69*GA + 1.93*BN + 2.61*BN + (3*CU*4.25) + 1.75*JI + 0.95*BR + 1.84*CR
+Sep28_24 = EG + 1.38*CR + PN + OL + PB + GY + LCC + (2*CU*4.25) + 2.30*BN + 2.87*BN + SB + SN + 1.43*BP + 2.59*GG
+Oct2_24 = GY + 1.11*BP + 3.59*GA + 5.57*BN + SB + 1.94*CR + 2.45*SP + (1*CU*4.25) + 2.03*JI
+Oct5_24 = GY + LCC + 2.51*GG + SN + 2.79*BN + 2.37*CR + 2.04*BP + (2*CU*4.25)
+Oct9_24 = LN + 2.68*GA + OL + SB + SN + 4.96*BN + 1.78*CR + 1.15*GP + 3.01*JI + LCC + GY
+Oct11_24 = (1*CU*4.25) + 2.08*GG + 1.55*GP + GY + PN + LCC + SB + 5.21*BN + 1.51*CR
+Oct14_24 = GY + LCC + 1.49*CR + 4.27*BN + WN + 2.58*GG
+Oct16_24 = GY + LCC + LN + OL + 2.83*BN + 3.19*BN + 1.64*CR
+Oct18_24 = PB + 2.79*SP + LCC + PN + 2.85*GG + 1.68*BN + GY + 2.58*BN + 3.07*GA + 0.75*BP + 4.49*JI
+Oct21_24 = GY + LCC + 3.03*BN + SN + SB + EG
+Oct22_24 = SB + 2.86*BN + 2.14*BN + 0.48*BP + 1.06*BR + 0.79*BP
+Oct26_24 = 4.73*GA + 2.71*JI + 2.49*CR + LCC + GY + 3.52*BN + 2.44*GG + (1*CU*4.25) + 0.89*BP
+Oct30_24 = PN + OL + 2.12*CR + LCC + GY + 2.43*GG + SN + 4.74*BN + PB
+Nov1_24 = PI + GY + LCC + (2*CU*4.25) + 2.82*SP + LN + EG + 2.04*CR + 3*BN + 2.74*BN + 2.47*GG
+Nov4_24 = 2.32*BN + LCC + 1.12*BP + (1*CU*4.25) + SB + GY
+Nov7_24 = 2.07*CR + 4.03*GA + (2*CU*4.25) + GY + LCC + OL + 2.32*BN + 3.62*BN
+Nov10_24 = 5.23*BN + PN + EG + PB + GY + SN + 1.23*CR + LCC
+Nov14_24 = 4.49*BN + 3.10*GG + LCC + GY + (1*CU*4.25) + 2.07*CR + 3.11*SP + GP + 2.34*BR
+Nov17_24 = OL + 4.09*GA + 4.33*BN + (1*CU*4.25) + 2.66*CR + LCC + LCC + GY
+Nov20_24 = EG + 2.53*GG + LCC + LCC + GY + SN + 5.56*BN + 1.17*BN + (1*CU*4.25) + 2.74*CR
+Nov24_24 = 2.27*GG + LN + SB + GY + (1*CU*4.25) + 1.86*CR + LCC + LCC + 4.55*BN
+Nov27_24 = PN + GY + LCC + SN + 2.31*GG + LCC + (1*CU*4.25) + 1.19*BP + 2.64*SP + 2.50*BN + 2.09*BN + 2.14*CR
+Dec1_24 = LN + OL + 4.19*NO + (1*CU*4.25) + 1.02*CR + 2.55*GG + LCC + LCC + GY + 2.54*BN + 2.50*BN
+Dec3_24 = EG + PN + GY + LCC + LCC + 4.76*BN + 1.78*CR + 5.12*NO + (1*CU*4.25)
+Dec6_24 = GY + SN + LN + 2.29*BN + 2.27*BN + 3.52*SP + LCC + LCC + PN + (1*CU*4.25) + 1.54*CR + 4.27*NO
+Dec9_24 = PN + LCC + GY + 2.08*CR + 4.53*BN + 1.85*GG + 5.67*NO
+Dec12_24 = 5.47*NO + 2.09*GG + PN + 4.88*BN + 1.26*BR + GY + LCC + LCC
+Dec16_24 = 1.83*CR + SN + LCC + GY + WN + 2.07*BN + LCC + (1*CU*4.25) + EG + 2.53*BN
+Dec19_24 = 5.38*NO + LCC + GY + LCC + (1*CU*4.25) + 1.8*GG + 2.02*CR + 2.88*BN + 2.78*BN
+Dec22_24 = 1.8*CB + 4.38*BN + LCC + GY + LCC + 2.1*CR + 4.29*NO + PN
+Dec24_24 = 1.57*CB + 3.5*SP + 1.88*BS + LCC + LN + GY + (1*CU*4.25) + SN + 4.48*BN
+Dec27_24 = GY + LCC + LCC + 1.76*CR + 4.97*NO + 5.15*BN + EG
+Dec30_24 = GY + PN + LN + MR + LCC + 5.38*BN + SN + (1*CU*4.25) + 1.52*BR + 1.45*JI
+Jan2_25 = 2.51*CR + 4.59*NO + 4.38*BN + GY + LCC + LCC + (1*CU*4.25)
+Jan5_25 = EG + LCC + 1.09*BP + PN + 5.26*NO + 2.07*BN + 2.87*BN + LCC + GY
+Jan8_25 = 5.54*NO + PN + LCC + LCC + GY + 5.60*BN + 1.96*CR + (1*CU*4.25) + SN
+Jan12_25 = LN + LN + 1.33*BR + 1.24*BP + 4.91*BN + 3.5*CB + PN + GY + LCC + LCC
+Jan15_25 = GY + LCC + LCC + WN + 2*BN + 2.05*BN + 1.82*CR + 5.46*NO + (1*CU*4.25)
+Jan19_25 = LCC + LCC + GY + 4.5*CB + 5.2*BN + 1.93*CR + 3.81*SP
+Jan21_25 = EG + GY + LCC + LCC + 2.96*BN + 4.3*NO + PN + LN + 1.58*BP
+Jan24_25 = GY + LN + LCC + LCC + 3.48*BN + 2.53*BN + 5.25*NO + MR + 2.13*CR + (1*CU*4.25)
+Jan27_25 = ZS*0.85 + 1.06*BP + SN + LCC + GY + PN + 2.05*CR
+Jan29_25 = GY + LCC + LCC + PN + 0.58*BP + 3.32*BN + 2.66*BN + (1*CU*4.25) + 1.78*CR
+
+
+
+Jun24 = June2_24 + June11_24 + June15_24 + June17_24 + June21_24 + June23_24 + June27_24
+July24 = July1_24 + July5_24 + July8_24 + July11_24 + July15_24 + July18_24 + July21_24 + July24_24 + July28_24 + July31_24
+Aug24 = Aug3_24 + Aug7_24 + Aug9_24 + Aug11_24 + Aug15_24 + Aug18_24 + Aug19_24 + Aug23_24 + Aug27_24 + Aug29_24
+Sep24 = Sep3_24 + Sep6_24 + Sep8_24 + Sep11_24 + Sep14_24 + Sep17_24 + Sep21_24 + Sep25_24 + Sep28_24
+Oct24 = Oct2_24 + Oct5_24 + Oct9_24 + Oct11_24 + Oct14_24 + Oct16_24 + Oct18_24 + Oct21_24 + Oct22_24 + Oct26_24 + Oct30_24
+Nov24 = Nov1_24 + Nov4_24 + Nov7_24 + Nov10_24 + Nov14_24 + Nov17_24 + Nov20_24 + Nov24_24 + Nov27_24
+Dec24 = Dec1_24 + Dec3_24 + Dec6_24 + Dec9_24 + Dec12_24 + Dec16_24 + Dec19_24 + Dec22_24 + Dec24_24 + Dec27_24 + Dec30_24
+Jan25 = Jan2_25 + Jan5_25 + Jan8_25 + Jan12_25 + Jan15_25 + Jan19_25 + Jan21_25 + Jan24_25 + Jan27_25 + Jan29_25
+
+
+print("6/2/2024", June2_24)
+print("6/11/2024", June11_24)
+print("6/15/2024", June15_24)
+print("6/17/2024", June17_24)
+print("6/21/2024", June21_24)
+print("6/23/2024", June23_24)
+print("6/27/2024", June27_24)
+print("7/1/2024", July1_24)
+print("7/5/2024", July5_24)
+print("7/8/2024", July8_24)
+print("7/11/2024", July11_24)
+print("7/15/2024", July15_24)
+print("7/18/2024", July18_24)
+print("7/21/2024", July21_24)
+print("7/24/2024", July24_24)
+print("7/28/2024", July28_24)
+print("7/31/2024", July31_24)
+print("8/3/2024", Aug3_24)
+print("8/7/2024", Aug7_24)
+print("8/9/2024", Aug9_24)
+print("8/11/2024", Aug11_24)
+print("8/15/2024", Aug15_24)
+print("8/18/2024", Aug18_24)
+print("8/19/2024", Aug19_24)
+print("8/23/2024", Aug23_24)
+print("8/27/2024", Aug27_24)
+print("8/29/2024", Aug29_24)
+print("9/3/2024", Sep3_24)
+print("9/6/2024", Sep6_24)
+print("9/8/2024", Sep8_24)
+print("9/11/2024", Sep11_24)
+print("9/14/2024", Sep14_24)
+print("9/17/2024", Sep17_24)
+print("9/21/2024", Sep21_24)
+print("9/25/2024", Sep25_24)
+print("9/28/2024", Sep28_24)
+print("10/2/2024", Oct2_24)
+print("10/5/2024", Oct5_24)
+print("10/9/2024", Oct9_24)
+print("10/11/2024", Oct11_24)
+print("10/14/2024", Oct14_24)
+print("10/16/2024", Oct16_24)
+print("10/18/2024", Oct18_24)
+print("10/21/2024", Oct21_24)
+print("10/22/2024", Oct22_24)
+print("10/26/2024", Oct26_24)
+print("10/30/2024", Oct30_24)
+print("11/1/2024", Nov1_24)
+print("11/4/2024", Nov4_24)
+print("11/7/2024", Nov7_24)
+print("11/10/2024", Nov10_24)
+print("11/14/2024", Nov14_24)
+print("11/17/2024", Nov17_24)
+print("11/20/2024", Nov20_24)
+print("11/24/2024", Nov24_24)
+print("11/27/2024", Nov27_24)
+print("12/1/2024", Dec1_24)
+print("12/3/2024", Dec3_24)
+print("12/6/2024", Dec6_24)
+print("12/9/2024", Dec9_24)
+print("12/12/2024", Dec12_24)
+print("12/16/2024", Dec16_24)
+print("12/19/2024", Dec19_24)
+print("12/22/2024", Dec22_24)
+print("12/24/2024", Dec24_24)
+print("12/27/2024", Dec27_24)
+print("12/30/2024", Dec30_24)
+print("1/2/2025", Jan2_25)
+print("1/5/2025", Jan5_25)
+print("1/8/2025", Jan8_25)
+print("1/12/2025", Jan12_25)
+print("1/15/2025", Jan15_25)
+print("1/19/2025", Jan19_25)
+print("1/21/2025", Jan21_25)
+print("1/24/2025", Jan24_25)
+print("1/27/2025", Jan27_25)
+print("1/29/2025", Jan29_25)
+
+
+print("June 2024", Jun24)
+print("July 2024", July24)
+print("August 2024", Aug24)
+print("September 2024", Sep24)
+print("October 2024", Oct24)
+print("November 2024", Nov24)
+print("December 2024", Dec24)
+print("January 2025", Jan25)
+
+
